aa7d79033d8700c233e1a6e9f9e7f04bcb860bea
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-25 15:54:45 +0300

    Added CustomUserManager class and interface

---
+++ Contracts/Services/Identity/ICustomUserManager.cs
@@ -0,0 +1,15 @@
+﻿using Database.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Contracts.Services.Identity
+{
+    public interface ICustomUserManager
+    {
+        Task<bool> CheckPasswordAsync(ApplicationUser user, string password);
+        Task<ApplicationUser> FindByIdAsync(string id);
+        Task<ApplicationUser> FindByNameAsync(string name);
+        Task<ApplicationUser> GetUserAsync(ClaimsPrincipal claimsPrincipal);
+        Task<IdentityResult> CreateAsync(ApplicationUser user, string password);
+    }
+}
---
+++ ServiceLayer/Identity/UserManager/CustomUserManager.cs
@@ -0,0 +1,44 @@
+﻿using Contracts.Services.Identity;
+using Database.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Services.Identity.UserManager
+{
+    public class CustomUserManager : ICustomUserManager
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CustomUserManager(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+
+        }
+
+        public async Task<ApplicationUser> FindByIdAsync(string id)
+        {
+            return await userManager.FindByIdAsync(id);
+        }
+
+        public async Task<ApplicationUser> FindByNameAsync(string name)
+        {
+            return await userManager.FindByNameAsync(name);
+        }
+
+
+        public async Task<ApplicationUser> GetUserAsync(ClaimsPrincipal claimsPrincipal)
+        {
+            return await userManager.GetUserAsync(claimsPrincipal);
+        }
+
+        public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            return await userManager.CheckPasswordAsync(user, password);
+        }
+
+        public async Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+        {
+           return await userManager.CreateAsync(user, password);
+        }
+    }
+}
