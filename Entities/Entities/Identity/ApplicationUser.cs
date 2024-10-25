7dd4ef82cbb2cf8e49a571d1eaadc0afd94477d1
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-25 15:55:07 +0300

    Updated Application User to make Email and PhoneNumber nullable

--- Entities/Entities/Identity/ApplicationUser.cs
+++ Entities/Entities/Identity/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Database.Entities.Identity
 {
@@ -7,5 +8,8 @@ namespace Database.Entities.Identity
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string LastName { get; set; }
+        public override string? Email { get; set; }
+        public override string? NormalizedEmail { get; set; }          
+        public override string? PhoneNumber { get; set; }
     }
 }
