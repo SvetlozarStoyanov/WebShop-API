d5f2d2199ed7e9cfc6dc9846c12f7146cdcc46aa
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-25 15:55:32 +0300

    Added extension to get logged in User Id

---
+++ WebShop/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebShop.Extensions
+{
+
+    [ApiController]
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
