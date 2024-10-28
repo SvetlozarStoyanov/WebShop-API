d0b0bf7feae63fd4620f2dca7d993e1108fad1a3
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-28 15:36:55 +0200

    Removed unnecessary attribute

--- WebShop/Extensions/ClaimsPrincipalExtensions.cs
+++ WebShop/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 
 namespace WebShop.Extensions
 {
-
-    [ApiController]
     public static class ClaimsPrincipalExtensions
     {
         public static string GetId(this ClaimsPrincipal claimsPrincipal)
