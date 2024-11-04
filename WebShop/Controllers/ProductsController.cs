197b3f4f74997913d5969b839ac75cc35cc835eb
SvetlozarStoyanov <svetlio214@gmail.com>
2024-11-04 13:00:43 +0200

    Added controller with actions for returning all and filtered products

---
+++ WebShop/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Contracts.Services.Entity.Products;
+using Microsoft.AspNetCore.Mvc;
+using Models.Dto.Products;
+
+namespace WebShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await productService.GetAllProductsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("filter")]
+        public async Task<IActionResult> Filter([FromQuery]ProductsQueryInputDto queryInputDto)
+        {
+            var queryOutputDto = await productService.GetFilteredProductsAsync(queryInputDto);
+
+            return Ok(queryOutputDto);
+        }
+    
+    }
+}
