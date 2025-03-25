e380c259c92b80e70c854781bbdb86a591f64df5
SvetlozarStoyanov <svetlio214@gmail.com>
2025-03-25 12:42:10 +0200

    Added new manager classes

---
+++ Contracts/Services/Managers/ApplicationUsers/IApplicationUserManager.cs
@@ -0,0 +1,41 @@
+﻿using Database.Entities.Identity;
+using Models.Common;
+
+namespace Contracts.Services.Managers.ApplicationUsers
+{
+    /// <summary>
+    /// Performs <see cref="ApplicationUser"/> related operations
+    /// </summary>
+    public interface IApplicationUserManager
+    {
+        /// <summary>
+        /// Returns <see cref="true"/> if <paramref name="userName"/> is taken by another <see cref="ApplicationUser"/>
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns><see cref="bool"/> result</returns>
+        Task<bool> IsUserNameTakenAsync(string userName);
+
+        /// <summary>
+        /// Returns all taken usernames
+        /// </summary>
+        /// <returns><see cref="IEnumerable"/> of type <see cref="string"/></returns>
+        Task<IEnumerable<string>> GetAllUserNamesAsync();
+
+        /// <summary>
+        /// Gets <see cref="ApplicationUser.ProfilePicture"/> of <see cref="ApplicationUser"/> 
+        /// with id: <paramref name="userId"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="OperationResult"/> with data - <see cref="string"/></returns>
+        Task<OperationResult<string?>> GetProfilePictureAsync(string userId);
+
+        /// <summary>
+        /// Updates <see cref="ApplicationUser.ProfilePicture"/> ,of <see cref="ApplicationUser"/> with id :
+        /// <paramref name="userId"/>, with <paramref name="profilePicture"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="profilePicture"></param>
+        /// <returns><see cref="OperationResult"/> result</returns>
+        Task<OperationResult> UpdateProfilePictureAsync(string userId, string? profilePicture);
+    }
+}
---
+++ Contracts/Services/Managers/Products/IProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Models.Dto.Products.Input;
+using Models.Dto.Products.Output;
+
+namespace Contracts.Services.Managers.Products
+{
+    /// <summary>
+    /// Performs <see cref="Product"/> related operations
+    /// </summary>
+    public interface IProductManager
+    {
+        /// <summary>
+        /// Returns all <see cref="Product"/>
+        /// </summary>
+        /// <returns><see cref="IEnumerable"/> of type <see cref="ProductListDto"/></returns>
+        Task<IEnumerable<ProductListDto>> GetAllProductsAsync();
+
+        /// <summary>
+        /// Retuns <see cref="Product"/> which match the criteria given by <paramref name="productsQueryInputDto"/>
+        /// </summary>
+        /// <param name="productsQueryInputDto"></param>
+        /// <returns><see cref="ProductsQueryOutputDto"/> productsQueryOutputDto</returns>
+        Task<ProductsQueryOutputDto> GetFilteredProductsAsync(ProductsQueryInputDto productsQueryDto);
+    }
+}
